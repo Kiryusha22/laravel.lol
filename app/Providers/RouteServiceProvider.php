1de0299915ba9571a958e73aa5c814b2f6374228
Kiruysha22 <bondarenkokirill87@gmail.com>
2023-11-11 16:13:33 +0700

    Изучили маршрутизацию

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,5 +36,8 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+
+        /* Глобальное ограничени на все параметры id в маршрутах*/
+        Route::pattern('id', '[0-9]+');
     }
 }
